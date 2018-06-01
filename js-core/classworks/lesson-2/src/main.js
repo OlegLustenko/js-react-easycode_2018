989e7700a320c54bb6d2ebd658dff9ee00b9ee95
Oleg Lustenko <oleglustenko@gmail.com>
2018-06-01 21:19:07 +0300

    lesson2

---
+++ js-core/classworks/lesson-2/src/main.js
@@ -0,0 +1,150 @@
+// const oldString = 'old \'string\''
+// const commonString = 'common string'
+//
+// const yearOld = 1989
+// const es6String = `something here`
+//
+// // Oleh born in 1989;
+//
+// // console.log(myName + ' born in ' + yearOld);
+// // console.log(`${myName} born in ${yearOld}`);
+//
+// /*
+//    отобразите строку на двух рядках
+//    используя ES6 интерполяцию
+// */
+// console.log('asd' + '\nqwe')
+// console.log(`asd
+// qwe`)
+//
+// const myName = 'oleh'
+// myName[0].toUpperCase()
+// const myNameUpperCase = myName.toUpperCase()
+//
+// console.log('WWW.GOOGLE.COM'.toLowerCase().toUpperCase().toLowerCase())
+// console.log(myName[0].toUpperCase() + myName.slice(1))
+// console.log('---->', myName.slice(0, 3))
+//
+// /*-> Youtube */
+// const youtube = 'Yoube'
+//
+// /*
+// * 1. Взять первые 3 символа
+// * 2. К первым взятым символам добавить 'tu'
+// * 3. взять 'be' у переменной youtube
+// * 4. добавить к 1,2 'be'
+// *
+// * */
+//
+// // 1. Взять первые 3 символа
+// // const firstSymbols = youtube.slice(0, 3)
+// // // 2. К первым взятым символам 'tu'
+// // const tutu = 'tu';
+// // // 3. взять 'be' у переменной youtube
+// // const secondSymbols = youtube.slice(-2);
+// //
+// // console.log(youtube.slice(0, 3) + 'tu' + youtube.slice(-2));
+//
+// /* '01@12@1955' преобразовать к  ['01', '12', '1955']
+// *
+// *
+// * */
+//
+// // console.log('01@@@12@@@1955'.split('@'))
+//
+// /*
+// *
+// * 'Yahoo'.replace('o', '@')
+// *  -> yah@@
+// *
+// * */
+//
+// // console.log('Yahoo'.replace('o', '@').replace('o', '@'))
+//
+// console.log('The Avengers'.includes('Ave'))
+//
+// console.log('easy length: ', 'easy'.length)
+// console.log('@_@-'.repeat(10))
+//
+// console.log(10 + 20) // 30
+// console.log(10 / 1) // 10
+// console.log(20 * 5) // 100
+//
+// console.log(20 - 10) // 10
+// console.log(10 + 10 - 20) // 0
+// console.log(2 * 5 + 10) // 20
+//
+// console.log('' + 10);
+//
+// // undefined
+// // NaN
+// // 10
+// // '10'
+//
+// // console.log(' ' + 10);
+// console.log([] + [])
+// console.log({} + [])
+//
+// console.log(10 == 10) // true
+// console.log('10' == 10) // true
+// console.log('10' === 10) //false
+//
+// console.log('empty string', '' == true) //
+// console.log('something in string', '') //
+//
+// console.log(15 % 10)
+//
+// const year2018 = 2018
+//
+// /*
+// * 2018 -> отобразите в консоле 18
+// *
+// * */
+//
+// const myArrayWithNumbers = [1, 2, 3, 100, -1, -Infinity]
+//
+// console.log(Math.max(1, 2, 3, 100, -1, Infinity))
+// console.log('Infinity friday night: ', Math.min(...myArrayWithNumbers))
+
+/*
+* напишите свое имя в маленком регистре
+* а потом отобразите в консоле ваше имя с заглавной буквы
+*
+* */
+
+var userAge = 33
+var userName = 'Jesus'
+var userSkills = ['HTML', 'PHP', 'Joomla']
+const user = {}
+var car = undefined
+
+console.log(typeof userAge)
+console.log(typeof userName)
+
+// console.log('isArray ? ', Array.isArray(userSkills));
+
+// console.log(typeof user)
+// console.log()
+// console.log(typeof car)
+// console.log(typeof NaN)
+
+const easyArray = [10, 20, 30]
+
+const easyWord2 = 'easy';
+// console.log(student)
+//
+// student.salary = '3000$'
+// student['offers'] = ['facebook', 'google']
+// student.offers = ['facebook', 'google']
+
+console.log('easyArray length: ', easyArray.length)
+console.log('easyArray length: ', easyArray[100])
+console.log()
+// console.log(easyWord2[0])
+
+
+
+
+
+
+
